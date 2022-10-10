e95df3e02aa3dc19754be93e5304c460675eb234
nimesh maharjan <maharjannimesh11@gmail.com>
2022-10-10 10:49:06 +0545

    feat(checkbox): animate checkbox

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Pressable } from "react-native";
+
 import {
   Text,
   Box,
@@ -11,8 +13,13 @@ import {
 } from "native-base";
 
 import ThemeToggler from "../components/theme-toggle";
+import AnimatedCheckbox from "../components/animated-checkbox";
 
 const MainScreen = () => {
+  const [checked, setChecked] = React.useState(false);
+  const handlePressCheckbox = React.useCallback(() => {
+    setChecked((prev) => !prev);
+  }, []);
   return (
     <Center
       _dark={{ bg: "blueGray.900" }}
@@ -21,6 +28,11 @@ const MainScreen = () => {
       flex={1}
     >
       <VStack space={5} alignItems="center">
+        <Box w="100px" h="100px">
+          <Pressable onPress={handlePressCheckbox}>
+            <AnimatedCheckbox {...{ checked }}></AnimatedCheckbox>
+          </Pressable>
+        </Box>
         <Box>
           <Text>Hello World</Text>
         </Box>
